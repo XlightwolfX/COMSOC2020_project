34fdd48af274628ee169f0d8dc6685d9cca047c8
David Cerny <david.cerny.lw@gmail.com>
2020-05-05 17:27:12 +0200

    add function for graph edit distance

---
+++ utils.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+
+def get_dag_edit_distance(graph1, graph2, optim=False):
+    """
+    Calculate graph edit distance: minimum sequence of node and edge operations
+    for graph1 and graph2 to become isomorphic.
+    graph1, graph2 [PartialOrder]
+    optim [bool] - if TRUE then use approximation algorithm of GED
+    """
+    G1 = nx.DiGraph(graph1.partial)
+    G2 = nx.DiGraph(graph2.partial)
+
+    dist = 0.0
+    if optim:
+        for i in nx.algorithms.similarity.optimize_graph_edit_distance(G1, G2):
+            dist = i
+    else:
+        dist = nx.algorithms.similarity.graph_edit_distance(G1, G2)
+
+    return dist
