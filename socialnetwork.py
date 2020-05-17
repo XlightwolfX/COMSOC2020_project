9ea12ed9ed33b27bd4f6ac693ed27bc8ede94fde
olinarr <olivieronardi@gmail.com>
2020-05-17 16:47:27 +0200

    improved structure of social network code. Added first draft of experiment.

--- socialnetwork.py
+++ socialnetwork.py
@@ -2,14 +2,16 @@ from partialorders import PartialOrder
 from voter import Voter
 from networks import generate_graphs
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 from votingrules import VotingRules
 import networkx as nx
+from tqdm import tqdm
+from utils import regret
 
 class SocialNetwork:
     """Class representing the social network"""
 
-    def __init__(self, dataset, graph = 'path', strategy = 'liquid'):
+    def __init__(self, dataset, graph = 'scale-free'):
         """ Initialize the Social Network.
 
         Parameters:
@@ -23,7 +25,7 @@ class SocialNetwork:
         for strict, count in zip(dataset.preferences, dataset.counts):
             for _ in range(count):
                 # TODO: parametrize the indiff level
-                partial = PartialOrder.generate_from_strict(strict, 0.2)# random.choice([0, 0.3, 1]))
+                partial = PartialOrder.generate_from_strict(strict, random.choice([0, 0.2, 1]))
                 voter = Voter(partial, strict)
                 self.id2voter[voter_id_count] = voter
                 voter_id_count += 1
@@ -37,13 +39,6 @@ class SocialNetwork:
         else:
             raise NotImplementedError("Graphs can be either a string (random generation strategy) or dictionaries.")
 
-        # data structure to memorize the delegation graph
-        # will be filled in the pick_delegations func
-        self.delegations = dict()
-        # data structure to memorize the votes
-        # will be filled in the cast_vote func
-        self.votes = dict()
-
     def getNeighbours(self, voter_id):
         """ Returns a list of neighbours for a voter 
 
@@ -55,13 +50,19 @@ class SocialNetwork:
 
         return list(self.graph.successors(voter_id))
 
-    def pick_delegations(self, strategy = 'liquid'):
-        """ Pick the delegations for each voter. In this function, the data structure self.delegations will be filled.
+    def _pick_delegations(self, paradigm = 'liquid', print_delegations = False):
+        """ Pick the delegations for each voter. 
 
         Parameters:
-        strategy (str): direct voting, proxy voting or liquid democracy? """
+        paradigm (str): direct voting, proxy voting or liquid democracy? 
+        print_delegations (bool): whether to print the selected delegations
 
-        if strategy == 'liquid':
+        Returns:
+        dict(int, [int, NoneType]): a mapping from a voter id to another """
+
+        delegations = dict()
+
+        if paradigm == 'liquid':
 
             # for every voter...
             for voter_id, voter in self.id2voter.items():
@@ -70,72 +71,93 @@ class SocialNetwork:
                 # and the neighbours themselves
                 neighbours = [self.id2voter[neighbour_id] for neighbour_id in neighbours_id]
                 # construct delegations. This will be a list.
-                delegations = voter.delegate(neighbours_id, neighbours)
+                delegation = voter.delegate(neighbours_id, neighbours)
                 # if it is empty, we do not delegate. Signal this with a None.
-                if len(delegations) == 0:
-                    delegations = None
+                if len(delegation) == 0:
+                    delegation = None
                 # otherwise, pick a random delegation from the list (notice that, if it is of length 1, it's simply the only element)
                 else:
-                    delegations = random.choice(delegations)
+                    delegation = random.choice(delegation)
 
-                self.delegations[voter_id] = delegations
+                delegations[voter_id] = delegation
 
-        elif strategy == 'direct':
+        elif paradigm == 'direct':
             for voter_id in self.id2voter.keys():
-                self.delegations[voter_id] = None
-        elif strategy == 'proxy':
+                delegations[voter_id] = None
+        elif paradigm == 'proxy':
             raise NotImplementedError("Proxy voting not yet available")
         else:
             raise NotImplementedError("This delegation strategy does not exist.")
 
-    def cast_votes(self):
-        """ Assign to each voter a vote. In this function, the data structure self.votes will be filled. """
+        if print_delegations:
+            for i, j in delegations.items():
+                if j is not None:
+                    print(f"{i} -> {j}")
 
-        # If it was not already done, pick the delegation
-        if not self.delegations:
-            self.pick_delegations()
+        return delegations
 
-        # TODO abstain instead
+    def _cast_votes(self, paradigm = 'liquid', print_delegations = False):
+        """ Assign to each voter a vote. 
+
+        Parameters:
+        paradigm (str): which paradigm? (liquid, direct, proxy...) 
+        print_delegations (bool): whether to print the selected delegations
+
+        Returns:
+        dict(int, list(int)): a mapping from a voter id to a preference """
+
+        # pick the delegation
+        delegations = self._pick_delegations(paradigm, print_delegations)
+
+        votes = dict()
 
         # for every voter:
         for voter_id in self.id2voter.keys():
-            self.votes[voter_id] = self._retrieve_vote(voter_id)
+            votes[voter_id] = self._retrieve_vote(voter_id, votes, delegations)
 
-    def _retrieve_vote(self, voter_id):
+        return votes
+
+    def _retrieve_vote(self, voter_id, votes, delegations):
         """ Recursvely get the voter_id's vote from delegation graph 
 
         Parameters:
         voter_id (int): a voter
+        votes dict(int, list(int)): a mapping from a voter id to a preference 
+        delegations (dict(int, [int, NoneType]): delegations from voter id to another
 
         Returns:
         list(int): a ballot """
 
         # if this person already voted, simply return the vote.
-        if voter_id in self.votes.keys():
-            return self.votes[voter_id]
+        if voter_id in votes.keys():
+            return votes[voter_id]
         # otherwise...
         # if he does not delegate, 
         # save his vote and return it
-        elif self.delegations[voter_id] is None:
-            self.votes[voter_id] = self.id2voter[voter_id].cast_random_vote()
-            return self.votes[voter_id]
+        elif delegations[voter_id] is None:
+            votes[voter_id] = self.id2voter[voter_id].cast_random_vote()
+            return votes[voter_id]
         # if he does, get the vote (recursvely) of the person he delegates to
         # we save it as ours and return it
         else:
-            next_vote = self._retrieve_vote(self.delegations[voter_id])
-            self.votes[voter_id] = next_vote
+            next_vote = self._retrieve_vote(delegations[voter_id], votes, delegations)
+            votes[voter_id] = next_vote
             return next_vote
 
-    def preference_list(self):
+    def get_preferences(self, paradigm = 'liquid', print_delegations = False, pretty_print = False):
         """ Return the preference list of the social network. This function 
         creates the delegations, casts the votes and returns the preference lists.
+
+        Parameters:
+        paradigm (str): which paradigm? (liquid, direct, proxy...)
+        print_delegations (bool): whether to print the selected delegations
+        pretty_print (bool): print the preferences nicely
 
         Returns:
         list(int), list(int): all the ballots with their counts """
 
-        # if it was not done already, cast the votes
-        if not self.votes:
-            self.cast_votes()
+        # cast the votes
+        votes = self._cast_votes(paradigm, print_delegations)
 
         # support functions because a list is not hashable
         # this enables us to use the Counter
@@ -143,7 +165,7 @@ class SocialNetwork:
         to_list = lambda key: [int(val) for val in key.split()]
 
         # transform the votes into keys, so we can use the counter
-        keys = map(to_key, self.votes.values())
+        keys = map(to_key, votes.values())
 
         # count the preferences
         counter = Counter(keys)
@@ -158,13 +180,20 @@ class SocialNetwork:
 
         assert (sum(counts) == len(self.id2voter.keys()))
 
+        if pretty_print:
+            self.pretty_print_pref(preferences, counts)
+
         return preferences, counts
 
-    def pretty_print_pref(self):
-        preferences, counts = self.preference_list()
-        for pref, count in sorted(zip(preferences, counts), key = lambda x: -x[1]):
-            print(f"{count} voters think {pref}")
+    def pretty_print_pref(self, preferences, counts):
+        """ Print cutely the preferences of the electorate
+        
+        Parameters:
+        preferences (list(list(int))): ballots
+        counts (list(int)): one per each ballot; number of people who submitted that ballot """
 
+        for pref, count in sorted(zip(preferences, counts), key = lambda x: -x[1]):
+            print(f"{count} voters think that {pref}")
 
 
 if __name__ == '__main__':
@@ -172,15 +201,21 @@ if __name__ == '__main__':
 
     from dataset import Dataset
 
-    data = Dataset(source='random', rand_params=[4, 500])
+    data = Dataset(source='random', rand_params=[4, 300])
     SN = SocialNetwork(data)
-    preferences, counts = SN.preference_list()
-    SN.pretty_print_pref()
+    import numpy as np
+    for paradigm in ['liquid', 'direct']:
+        regrets = defaultdict(lambda : [])
+        for _ in tqdm(range(50)):
+            preferences, counts = SN.get_preferences(paradigm)
 
-    print([f'{voter}->{delegation}' for voter, delegation in SN.delegations.items()
-        if delegation is not None])
-    
+            # min == lexicographic rule
+            for rule in VotingRules.rules:
+                winner = min(VotingRules.elect(rule, preferences, counts))
+                regrets[rule].append(regret(winner, preferences, counts))
 
-    print(f'Borda winner: {VotingRules.elect_borda(preferences, counts)}')
-    print(f'Plurality winner: {VotingRules.elect_plurality(preferences, counts)}')
-    print(f'Copeland winner:{VotingRules.elect_copeland(preferences, counts)}')
+        print(f'## average regret of `{paradigm}` paradigm ##')
+        for rule, val in regrets.items():
+            val = np.array(val)
+            print(f'{rule}: {val.mean():.4f} (+-{val.std():.4f})')
+        print(f'#####')
