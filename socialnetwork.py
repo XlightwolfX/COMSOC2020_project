b956b1b15c3dd5b4318fd64288baa3ea97ced4b9
olinarr <olivieronardi@gmail.com>
2020-05-19 10:26:10 +0200

    added separate experiments file

--- socialnetwork.py
+++ socialnetwork.py
@@ -27,7 +27,7 @@ class SocialNetwork:
             self.id2voter = dict()
             for strict, count in zip(dataset.preferences, dataset.counts):
                 for _ in range(count):
-                    # TODO: parametrize the indiff level
+                    # TODO: parametrize the indiff level!!
                     partial = PartialOrder.generate_from_strict(strict, random.choice([0, 0.2, 1]))
                     voter = Voter(partial, strict)
                     self.id2voter[voter_id_count] = voter
@@ -145,14 +145,14 @@ class SocialNetwork:
             votes[voter_id] = next_vote
             return next_vote
 
-    def get_preferences(self, paradigm = 'liquid', print_delegations = False, pretty_print = False):
+    def get_preferences(self, paradigm = 'liquid', print_delegations = False, print_preferences = False):
         """ Return the preference list of the social network. This function 
         creates the delegations, casts the votes and returns the preference lists.
 
         Parameters:
         paradigm (str): which paradigm? (liquid, direct, proxy...)
         print_delegations (bool): whether to print the selected delegations
-        pretty_print (bool): print the preferences nicely
+        print_preferences (bool): print the preferences nicely
 
         Returns:
         list(int), list(int): all the ballots with their counts """
@@ -181,7 +181,7 @@ class SocialNetwork:
 
         assert (sum(counts) == len(self.id2voter.keys()))
 
-        if pretty_print:
+        if print_preferences:
             self.pretty_print_pref(preferences, counts)
 
         return preferences, counts
@@ -195,96 +195,3 @@ class SocialNetwork:
 
         for pref, count in sorted(zip(preferences, counts), key = lambda x: -x[1]):
             print(f"{count} voters think that {pref}")
-
-
-if __name__ == '__main__':
-    # test
-
-    # test-specific imports
-    import argparse
-    from dataset import Dataset
-    import numpy as np
-    from tqdm import tqdm
-    from utils import regret, partial_regret
-    from votingrules import VotingRules
-    from collections import defaultdict
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--alternatives', type=int, default=4, help='Number of alternatives.')
-    parser.add_argument('--voters', type=int, default=1000, help='Number of voters.')
-    parser.add_argument('--experiments', type=int, default=500, help='Number of experiments.')
-    parser.add_argument('--experiment_type', type=str, default='random_graph', help='Which experiments?')
-    parser.add_argument('--graph_type', type=str, default='path', help='Type of graph to be generated')
-    parser.add_argument('--print_graph', action='store_true', help='Print the generated graph')
-    parser.add_argument('--use_partial_regret', action='store_true', help='Use the alternative metric of partial regret instead.')
-
-    args = parser.parse_args()
-
-    if args.experiment_type == 'random_graph':
-        data = Dataset(source='random', rand_params=[args.alternatives, args.voters])
-        true_preferences, true_counts = data.preferences, data.counts
-        SN = SocialNetwork(strategy = 'dataset_and_random_edges', dataset = data, graph_generation = args.graph_type, print_graph = args.print_graph)
-
-    elif args.experiment_type == 'case_study_star':
-        # this is a simple case study where everyone is connected to a center
-        # everybody has a true strict order 1,2,3,4
-        # only the center knows it
-        center_voter = Voter(PartialOrder({1:[2,3,4], 2:[3,4], 3:[4], 4:[]}), [1,2,3,4])
-        id2voter = {0: center_voter}
-        graph = {0: []}
-        import itertools
-        for i, ll in enumerate(list(itertools.permutations([1, 2, 3, 4]))):
-            ll = list(ll)
-            if ll != [1,2,3,4]:
-                id2voter[i] = Voter(PartialOrder({1:[], 2:[], 3:[], 4:[]}), [1,2,3,4])
-                graph[i] = [0]
-
-        true_preferences = [voter.strict for voter in id2voter.values()]
-        true_counts = [1] * len(true_preferences)
-        SN = SocialNetwork(strategy = 'from_voter_graph', id2voter = id2voter, graph = graph, print_graph = args.print_graph)
-
-    elif args.experiment_type == 'typed_graph':
-        from typed_graph import generate_typed_graph
-
-        id2voter, graph = generate_typed_graph(N = args.voters)
-
-        true_preferences = [voter.strict for voter in id2voter.values()]
-        true_counts = [1] * len(true_preferences)
-        SN = SocialNetwork(strategy = 'from_voter_graph', id2voter = id2voter, graph = graph, print_graph = args.print_graph)
-
-    else:
-        raise NotImplementedError('This experiment does not exist')
-    
-    # comparing direct and liquid
-    for paradigm in ['direct', 'liquid']:
-        # datastructure used to compute avg regret and distribution of winners
-        regrets = defaultdict(lambda : [])
-        winners = defaultdict(lambda : 0)
-        # repeat the experiment N times
-        for _ in tqdm(range(args.experiments)):
-            # run the delegation mechanism, assign a preference to everybody a count the ballots
-            SN_preferences, SN_counts = SN.get_preferences(paradigm)
-
-            # per every rule...
-            for rule in VotingRules.rules:
-                # min == lexicographic rule
-                winner = VotingRules.elect(rule, SN_preferences, SN_counts, tiebreaking = min)
-                winners[winner] += 1
-                # note: if here we use SN_preferences, SN_counts we compute the regret
-                # on the "reported" ballots not the TRUE preferences.
-                # Interestingly, if here we use SN_preferences, SN_counts, liquid democracy
-                # vastly outperform direct...
-                # TODO: investigate this!
-
-                if args.use_partial_regret:
-                    regrets[rule].append(partial_regret(winner, SN.id2voter.values()))
-                else:
-                    regrets[rule].append(regret(winner, true_preferences, true_counts))
-
-        print(f"## average {'partial ' if args.use_partial_regret else ''}regret of `{paradigm}` paradigm ##")
-        for rule, val in regrets.items():
-            val = np.array(val)
-            print(f'{rule}: {val.mean():.4f} (+-{val.std():.4f})')
-        # TODO main issue: it seems like using either mechanism does not make any difference in result.
-        print(f'\ncounts of winners: {[(wnnr, cnt) for wnnr, cnt in winners.items()]}')
-        print(f'#####')
