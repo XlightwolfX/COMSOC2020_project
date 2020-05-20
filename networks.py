00438e1e96d157870789fcdf9068efe7b67879cb
olinarr <olivieronardi@gmail.com>
2020-05-20 18:35:57 +0200

    add small-world graph

--- networks.py
+++ networks.py
@@ -13,15 +13,14 @@ def random_network(n, p, seed):
 
     return nx.DiGraph(graph)
 
-def generate_graphs(num_voters, num_graphs, gtype='scale-free', seed=42):
+def generate_graphs(num_voters, num_graphs, gtype='scale-free', seed=42, degree = 4, prob = 0.5):
     gtypes = {
-        'scale-free': nx.scale_free_graph,
-        'path': lambda n, seed: nx.generators.classic.path_graph(n, create_using = nx.classes.multidigraph.MultiDiGraph),
+        'scale-free': nx.scale_free_graph(num_voters, seed=seed),
+        'path': lambda : nx.generators.classic.path_graph(num_voters, create_using = nx.classes.multidigraph.MultiDiGraph),
         # TODO better control of parameters
-        'random': lambda n, seed: random_network(n, 0.5, seed)
-        # these dont work but we need something similar
-        # 'regular' : lambda n, seed: nx.generators.random_graphs.random_regular_graph(n // 4, n, seed),
-        # 'small-world': lambda n, seed : connected_watts_strogatz_graph()
+        'random': lambda : random_network(num_voters, 0.5, seed = seed),
+        'regular' : lambda n, seed: nx.generators.random_graphs.random_regular_graph(n // 4, n, seed),
+        'small-world': lambda : nx.DiGraph(nx.generators.random_graphs.watts_strogatz_graph(num_voters, degree, prob, seed)),
         }
 
     if gtype not in gtypes:
@@ -31,4 +30,4 @@ def generate_graphs(num_voters, num_graphs, gtype='scale-free', seed=42):
 
     for i in range(num_graphs):
         seed = seed + 1 if seed is not None else None
-        yield g_func(num_voters, seed=seed)
+        yield g_func()
