8d2e66803438faa6ac2063af00c0681105e1ec88
zldell <zldell@git>
2020-05-23 00:27:01 +0200

    preflib voters

--- voter_type.py
+++ voter_type.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 class VoterTypes:
-    def __init__(self, num_types, gen_type='half_normal'):
-        self._gen_types = {'half_normal': self._half_normal_generator}
+    def __init__(self, num_types=2, gen_type='half_normal'):
+        self._gen_types = {
+                            'half_normal': self._half_normal_generator,
+                            'tshirt': self._tshirt_generator
+                          }
         self._num_types = num_types
         if gen_type not in self._gen_types:
             raise NotImplementedError('This generation type has not been implemented.')
@@ -15,6 +18,24 @@ class VoterTypes:
             self._pick_types()
             self._hn_values = np.array([self._half_normal_pdf(x) for x in range(4)])
 
+        if gen_type == 'tshirt':
+            self._tshirt_init()
+
+    def _tshirt_init(self, path='dataset/tshirt.soc'):
+        with open(path, 'r') as f:
+            raw = f.readlines()
+            num_alternatives = int(raw[0].strip('\n'))
+            preference_profiles = [list(eval(x[2:].strip('\n'))) for x in raw[13:]]
+
+        four_alternatives = list(np.random.choice(range(1, num_alternatives + 1), 4, replace=False))
+        a_map = {a: i for i, a in enumerate(four_alternatives)}
+        self._preference_profiles = [[a_map[a] for a in pref if a in a_map] for pref in preference_profiles]
+
+    def _tshirt_generator(self):
+        p = np.random.randint(len(self._preference_profiles))
+        return self._preference_profiles[p]
+
+    # half_normal
     def _pick_types(self):
         all_types = [list(x) for x in permutations([1, 2, 3, 4])]
         total = len(all_types)
